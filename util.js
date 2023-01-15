669877a6cf8c48b892d5847265dd8835ccbfc00c
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2023-01-15 23:26:26 +0900

    [MOD] try-catch 추가

--- util.js
+++ util.js
@@ -1,7 +1,11 @@
 const baseURL = "http://43.201.103.199/"
 
 export const getData = async (url) => {
-  const response = await fetch(baseURL + url)
-  const data = response.json()
-  return data
+  try {
+    const response = await fetch(baseURL + url)
+    const data = response.json()
+    return data
+  } catch (e) {
+    console.error(e)
+  }
 }
